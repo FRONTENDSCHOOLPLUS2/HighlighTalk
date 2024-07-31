ef5441c74d8eec9dc11bfb09ec8e51ca5c9060bb
woojoung <ywj981217@naver.com>
2024-07-31 18:23:01 +0900

    Chore css 및 next 기초세팅

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <div>
+      <h3>{error.message}</h3>
+      <p>이 오류는 더 나은 서비스를 위한 첫걸음이에요. 조금만 기다려 주세요!</p>
+    </div>
+  );
+}
