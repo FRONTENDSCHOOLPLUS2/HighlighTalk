3cfc529fd0c5dacf32583d33b26e77f63ad4d516
여다희 <yeodahui.dev@gmail.com>
2024-08-07 22:14:00 +0900

    Fix: getTokenLength 모듈 import 수정 #30

--- src/utils/getTokenLength.ts
+++ src/utils/getTokenLength.ts
@@ -1,8 +1,7 @@
-import o200k_base from 'gpt-tokenizer/encoding/o200k_base';
+import { encode } from 'gpt-tokenizer/encoding/o200k_base';
 
 const getTokenLength = (text: string) => {
-  const api = o200k_base;
-  const encodedTokens = api.encode(text);
+  const encodedTokens = encode(text);
 
   return encodedTokens.length;
 };
