85d839c06c9af0ec5c7116cbc4a6684e5111a3c4
zldnlto <langchomi0@gmail.com>
2024-08-12 16:25:06 +0900

    Fix: 리뷰 반영_ 코드 스타일 수정 외 SCSS 변경사항 #36 - signupPage background 클래스 지역적으로 바인딩되도록 수정

--- src/app/(user)/signup/page.tsx
+++ src/app/(user)/signup/page.tsx
@@ -3,13 +3,13 @@ import './_SignupPage.scss';
 
 function SignupPage() {
   return (
-    <>
+    <div className="signup-page">
       <span className="background"></span>
       <div className="signup-page-container">
         <h1 className="title">회원가입</h1>
         <SignupForm />
       </div>
-    </>
+    </div>
   );
 }
 export default SignupPage;
--- src/serverActions/authAction.ts
+++ src/serverActions/authAction.ts
@@ -5,7 +5,7 @@ import { LoginFormType } from '@/types';
 
 import { redirect } from 'next/navigation';
 
-export async function signInWithCredentials(data: LoginFormType) {
+export const signInWithCredentials = async (data: LoginFormType) => {
   console.log('👼 User Login-> ', data);
   try {
     const result = await signIn('credentials', {
@@ -14,11 +14,11 @@ export async function signInWithCredentials(data: LoginFormType) {
       redirect: false,
     });
   } catch (error) {
-    // @ts-ignore-next-line // NOTE - 아직 해당 타입이 없어 무시합니다.
+    // @ts-expect-error-next-line // NOTE - 아직 해당 타입이 없어 무시합니다.
     throw new Error(error.cause.err.message);
   }
   redirect('/');
-}
+};
 
 export const signInWithSocial = async (provider: string) => {
   await signIn(provider, { redirectTo: '/' });
