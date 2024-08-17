6b49e193c31cc7ef401e1309aee29770eaef2b35
여다희 <yeodahui.dev@gmail.com>
2024-08-17 23:14:41 +0900

    Feat: 결과보기 버튼 눌렀을 때 현재 페이지 params 전달하도록 수정 #52

--- src/app/content/[id]/result/components/KakaoShareButton/KakaoShareButton.tsx
+++ src/app/content/[id]/result/components/KakaoShareButton/KakaoShareButton.tsx
@@ -11,28 +11,22 @@ declare global {
 function KakaoShareButton() {
   const handleKakaoShare = () => {
     const { Kakao } = window;
+    const path = location.pathname + location.search;
 
     if (Kakao.isInitialized()) {
       Kakao.Share.sendCustom({
         templateId: Number(process.env.NEXT_PUBLIC_KAKAO_SHARE_TEMPLATE_ID),
         templateArgs: {
-          // TODO: 결과보기 버튼 눌렀을 때 현재 페이지 params 전달하도록 수정
           // TODO: 추후 로그인한 유저 이름가져오게 변경
           user_name: '다희',
+          path: path,
         },
       });
     }
   };
 
   return (
-    <button
-      className="button-share-kakao"
-      type="button"
-      onClick={() => {
-        handleKakaoShare();
-        alert('kakao');
-      }}
-    >
+    <button className="button-share-kakao" type="button" onClick={handleKakaoShare}>
       카카오톡으로 공유하기
     </button>
   );
