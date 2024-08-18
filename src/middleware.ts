bee128af9df68fbec03dd9674c6958f51223bf93
zldnlto <langchomi0@gmail.com>
2024-08-18 21:39:26 +0900

    Feat: middleware를 통한 페이지 접근 제어 기능 구현 #73

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,29 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { auth } from './auth';
 
-// FIXME - 개발하면서 config, matchersForAuth에 접근 제어 페이지들 더 추가하기
-// NOTE - login 페이지 개발 완료 전 접근 편의를 위해 우선 주석 처리했습니다.
-const matchersForAuth = ['/signup/*', '/login/*'];
+// TODO - 현재 테스트 클릭시 게스트 상태이면 Login 안내 모달 없이 /login 페이지로 리다이렉트
+// 모달 띄워서 안내하도록 추가해야함
+
+const matchersForSignIn = ['/signup/*', '/login/*'];
+const matchersForAuth = ['/mypage/*', '/charge/*', '/freetest/*', '/lovetest/*', '/charge'];
 
 export const middleware = async (request: NextRequest) => {
-  // const mySession = await auth();
-  // const pathname = request.nextUrl.pathname;
-  // const isMatchForAuth = matchersForAuth.some((element) => element.includes(pathname));
-  // if (isMatchForAuth && mySession) {
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // }
+  const mySession = await auth();
+  const pathname = request.nextUrl.pathname;
+
+  // NOTE - 로그인 후 회원가입 및 로그인 페이지 접근 제어
+  const isMatchForSignIn = matchersForSignIn.some((element) => element.includes(pathname));
+  if (isMatchForSignIn && mySession) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // NOTE - 인증이 필요한 페이지 접근 제어
+  const isMatchForAuth = matchersForAuth.some((element) => element.includes(pathname));
+  if (isMatchForAuth) {
+    return mySession ? NextResponse.next() : NextResponse.redirect(new URL('/login', request.url));
+  }
 };
 
 export const config = {
-  matcher: ['/login', '/signup'],
+  matcher: ['/login', '/signup', '/mypage', '/freetest', '/lovetest/', '/charge'],
 };
