5135b87b9c649ec52ef8723dd3abb8f3584d5b62
여다희 <yeodahui.dev@gmail.com>
2024-08-19 15:03:01 +0900

    Feat: 결과페이지 주제 요약 렌더 3개로 제한 #52

--- src/app/(tests)/freetest/[id]/components/ThemeSection/ThemeSection.tsx
+++ src/app/(tests)/freetest/[id]/components/ThemeSection/ThemeSection.tsx
@@ -14,7 +14,7 @@ function ThemeSection({ data }: ThemeSectionPropType) {
       <h3 className="heading-3">대화 주제 TOP3</h3>
       <ul className="bubble-list">
         {data &&
-          data.map(({ title, content }, i) => (
+          data.slice(0, 3).map(({ title, content }, i) => (
             <li className="bubble-item" key={`bubble-${i}`}>
               <p className="bubble-title">{title}</p>
               <p className="bubble-text">{content}</p>
