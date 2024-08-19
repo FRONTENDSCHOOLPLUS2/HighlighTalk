57c1dcf9a0320e72559d13a9d9dfca2fb42c3263
여다희 <yeodahui.dev@gmail.com>
2024-08-19 18:32:13 +0900

    Fix: data.slice 오류 해결 #82

--- src/app/(tests)/freetest/[id]/components/ThemeSection/ThemeSection.tsx
+++ src/app/(tests)/freetest/[id]/components/ThemeSection/ThemeSection.tsx
@@ -8,13 +8,21 @@ interface ThemeSectionPropType {
 }
 
 function ThemeSection({ data }: ThemeSectionPropType) {
+  const getSlicedData = (data: { title: string; content: string }[]) => {
+    if (data.length > 3) {
+      return [...data].slice(0, 3);
+    } else {
+      return [...data];
+    }
+  };
+
   return (
     <section className="theme">
       <h2 className="heading-2">대화 분석 결과</h2>
       <h3 className="heading-3">대화 주제 TOP3</h3>
       <ul className="bubble-list">
         {data &&
-          data.slice(0, 3).map(({ title, content }, i) => (
+          getSlicedData(data).map(({ title, content }, i) => (
             <li className="bubble-item" key={`bubble-${i}`}>
               <p className="bubble-title">{title}</p>
               <p className="bubble-text">{content}</p>
