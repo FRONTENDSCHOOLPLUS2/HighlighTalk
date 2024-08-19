4b573535919401ad7ded586b079ae45e46b6922b
여다희 <yeodahui.dev@gmail.com>
2024-08-19 14:53:15 +0900

    Fix: 결과페이지 fetch를 통해 불러온 데이터로 화면 그리기 #52

--- src/app/(tests)/freetest/[id]/page.tsx
+++ src/app/(tests)/freetest/[id]/page.tsx
@@ -11,51 +11,36 @@ import './_FreeTestResultPage.scss';
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 const SERVER_URL = process.env.NEXT_PUBLIC_API_SERVER;
 
-// NOTE 채팅 주제 요약 더미데이터
-const chatThemeData = [
-  {
-    title: '대화내역 분석을 주제로 한 대화',
-    content:
-      '하이라이톡을 어떻게 개발할 것인가에 대해 의논했어요. 여다희님이 스타일링이 어렵다고 말하며 UI 개선을 제안하고, 김설하님과 윤우중님이 거부했어요.',
-  },
-  {
-    title: '대화내역 분석을 주제로 한 대화',
-    content:
-      '하이라이톡을 어떻게 개발할 것인가에 대해 의논했어요. 여다희님이 스타일링이 어렵다고 말하며 UI 개선을 제안하고, 김설하님과 윤우중님이 거부했어요.',
-  },
-  {
-    title: '대화내역 분석을 주제로 한 대화',
-    content:
-      '하이라이톡을 어떻게 개발할 것인가에 대해 의논했어요. 여다희님이 스타일링이 어렵다고 말하며 UI 개선을 제안하고, 김설하님과 윤우중님이 거부했어요.',
-  },
-];
+type EmotionType = '기쁨' | '슬픔' | '불안' | '놀람' | '분노';
 
-// NOTE 가장 많이 말한 사람 더미데이터
-const circlePackingData = [
-  { key: '김설하', value: 15 },
-  { key: '윤우중', value: 30 },
-  { key: '여다희', value: 25 },
-  { key: '정길용', value: 10 },
-  { key: '정현주', value: 9 },
-];
+interface FreeTestResultData {
+  topic: { summary: { title: string; content: string }[] };
+  mbti: {
+    analysis: {
+      names: string;
+      value: string;
+      reason: string;
+      ability: { energy: number; social: number; intelligence: number }[];
+      etc: string[];
+    }[];
+  };
+  talkCount: {
+    counts: {
+      [key: string]: number;
+    };
+  };
+  mostWords: {
+    topWords: {
+      [key: string]: number;
+    };
+  };
+  peoples: string[];
+  talkbangEmotion: {
+    [key in EmotionType]: number;
+  };
+}
 
-// NOTE 가장 많이 나온 단어 더미데이터
-const wordCloudData = [
-  { key: '중우', value: 10 },
-  { key: '짱이다', value: 15 },
-  { key: '울랄라', value: 30 },
-  { key: '얍', value: 100 },
-  { key: '응', value: 80 },
-  { key: '바빠', value: 41 },
-  { key: '나가라', value: 10 },
-  { key: '어떻게', value: 20 },
-  { key: '되는거지', value: 10 },
-  { key: '야', value: 40 },
-  { key: '아니야', value: 30 },
-  { key: '덤벼', value: 60 },
-];
-
-export async function getData(id: string) {
+export async function getData(id: string): Promise<FreeTestResultData | null> {
   try {
     const response = await fetch(`${SERVER_URL}/posts/${id}`, {
       headers: {
@@ -68,8 +53,8 @@ export async function getData(id: string) {
       return null;
     }
 
-    const data = await response.json();
-    return data;
+    const result = await response.json();
+    return result.item.extra.result;
   } catch (error) {
     console.error('Error fetching data:', error);
     return null;
@@ -77,11 +62,11 @@ export async function getData(id: string) {
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const item = await getData(params.id);
-  const extraItem = item?.item?.extra;
+  const data = await getData(params.id);
+  const { topic, mbti, talkCount, mostWords } = data!;
 
-  if (!extraItem) {
-    return <div>페이지를 찾을 수 없습니다.</div>;
+  if (!data) {
+    throw new Error('잘못된 접근입니다.');
   }
 
   return (
@@ -93,10 +78,10 @@ export default async function Page({ params }: { params: { id: string } }) {
         crossOrigin="anonymous"
         strategy="afterInteractive"
       ></Script>
-      <ThemeSection data={chatThemeData} />
-      <TopRatedTalkerSection data={circlePackingData} />
-      <MbtiSection data={extraItem.result} />
-      <PopularWordSection data={wordCloudData} />
+      <ThemeSection data={topic.summary} />
+      <TopRatedTalkerSection data={talkCount.counts} />
+      <MbtiSection data={mbti.analysis} />
+      <PopularWordSection data={mostWords.topWords} />
       <ShareSection />
     </article>
   );
