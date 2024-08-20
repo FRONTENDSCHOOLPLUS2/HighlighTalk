4b5b18d47970d5ddb8c98fe2989847afa005bcf1
zldnlto <langchomi0@gmail.com>
2024-08-20 10:15:40 +0900

    Design: 로그인, 회원가입 페이지 추가 스타일링 #81 - background + 캐릭터 이미지 추가, 타이틀 정렬

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,15 +1,14 @@
 import LoginForm from '@/components/Form/LoginForm/LoginForm';
 import './_LoginPage.scss';
+import { poppinsFont } from '@/utils/font';
 
 function LoginPage() {
   return (
-    <>
+    <div className="login-page-container">
       <span className="background"></span>
-      <div className="login-page-container">
-        <h1 className="title">로그인</h1>
-        <LoginForm />
-      </div>
-    </>
+      <h1 className={`${poppinsFont.className} title`}>로그인</h1>
+      <LoginForm />
+    </div>
   );
 }
 export default LoginPage;
