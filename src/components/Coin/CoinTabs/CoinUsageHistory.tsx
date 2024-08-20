7b3a453311706cf3bc68880c5ba7bc1a3a4d9758
zldnlto <langchomi0@gmail.com>
2024-08-20 23:50:21 +0900

    Feat: Coin Notice 영역 UI 구현, 결제목록 불러오기 기능 스캐폴딩 #69

--- src/components/Coin/CoinTabs/CoinUsageHistory.tsx
+++ src/components/Coin/CoinTabs/CoinUsageHistory.tsx
@@ -1,4 +1,46 @@
+'use client';
+import Button from '@/components/Button/Button';
+import { getSession, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+// TODO - order 컬렉션 불러오기
+
+const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+
 function CoinUsageHistory() {
-  return <div>CoinUsageHistory</div>;
+  const session = useSession();
+  const [orders, setOrders] = useState([]);
+
+  // FIXME -  결제 목록 내역을 불러오는 함수 (500 Error)
+
+  useEffect(() => {
+    const getOrderList = async () => {
+      try {
+        const response = await fetch(`${API_SERVER}/orders/`, {
+          headers: {
+            'client-id': `${CLIENT_ID}`,
+            Authorization: `Bearer ${session.data?.accessToken}`,
+          },
+        });
+
+        const result = await response.json();
+        console.log(result, '결과조회');
+
+        if (response.ok) {
+          setOrders(result.item);
+        } else {
+          console.log(result.message);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getOrderList();
+  }, [CLIENT_ID]);
+
+  return <div>{/* // TODO - 결제내역 결과 뿌리기  */}</div>;
 }
+
 export default CoinUsageHistory;
