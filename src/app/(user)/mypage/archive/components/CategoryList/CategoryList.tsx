7cbc7baf5568191bea571433cbc27bfe75c28941
여다희 <yeodahui.dev@gmail.com>
2024-08-20 15:23:30 +0900

    Feat: 분석 결과 보관함 UI 구현 #84

---
+++ src/app/(user)/mypage/archive/components/CategoryList/CategoryList.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Button from '@/components/Button/Button';
+import './_CategoryList.scss';
+import React, { useState } from 'react';
+
+function CategoryList() {
+  const [focusedIndex, setFocusedIndex] = useState<number>(0);
+  const categories = ['전체', '내 분석', '공유됨'];
+
+  const handleClick = (index: number) => {
+    setFocusedIndex(index);
+  };
+
+  return (
+    <ul className="list-categories">
+      {categories.map((name, index) => (
+        <li>
+          <Button
+            theme="black"
+            styleType={index === focusedIndex ? 'default' : 'tonal'}
+            rounded
+            onClick={() => {
+              handleClick(index);
+            }}
+          >
+            {name}
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CategoryList;
