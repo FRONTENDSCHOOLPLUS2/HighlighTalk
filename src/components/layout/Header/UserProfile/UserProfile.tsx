9c331b76be031bff8b4f7129aae91c9c02a174d7
zldnlto <langchomi0@gmail.com>
2024-08-22 01:31:13 +0900

    Feat: 태블릿 뷰 사이드바 기능 구현 #37

--- src/components/layout/Header/UserProfile/UserProfile.tsx
+++ src/components/layout/Header/UserProfile/UserProfile.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { Session } from 'next-auth';
-import './_UserProfile.scss';
+// import './_UserProfile.scss';
 
 import { IconProfile } from '@public/image';
 
@@ -14,8 +14,6 @@ function UserProfile({ userSession }: UserProfilePropType) {
   const router = useRouter();
   // NOTE - 세션 정보 브라우저 조회용으로 작성
   console.log('UserProfile_🪪 session', userSession);
-
-  console.log('UserProfile / session', userSession);
 
   const userName = userSession?.user?.name;
 
