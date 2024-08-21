604d9861740e27d2ea28b9ff41f2bb69589667e3
zldnlto <langchomi0@gmail.com>
2024-08-22 01:25:45 +0900

    Chore: Assets 추가 _ X 아이콘 #37

--- public/image/index.ts
+++ public/image/index.ts
@@ -3,6 +3,8 @@ import IconEdit from './icon_edit.svg';
 import IconGithub from './icon_github.svg';
 import IconInsta from './icon_insta.svg';
 import IconMail from './icon_mail.svg';
+import IconHamburger from './icon_hamburger.svg';
+import IconCross from './icon_cross.svg';
 import Coin from './coin.svg';
 
-export { IconProfile, IconEdit, Coin, IconGithub, IconInsta, IconMail };
+export { IconProfile, IconEdit, IconGithub, IconInsta, IconMail, IconHamburger, IconCross, Coin };
