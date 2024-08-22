ffa022fd50d4eee22e1f6710e7f4a29ee94c6d8e
여다희 <yeodahui.dev@gmail.com>
2024-08-22 16:37:54 +0900

    Feat: 결과 데이터 타입 정리 #85

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,3 +1,5 @@
+import { FreeTestData, LoveTestData } from './test';
+
 export interface User {
   _id: number;
   name: string;
@@ -17,16 +19,17 @@ export interface PostItem {
   type: string;
   title: string;
   content: string;
-  image: string;
+  image?: string;
   views: number;
   user: User;
   replies: string;
   createdAt: string;
   updatedAt: string;
-  seller_id: number | null;
-  repliesCount: number;
-  product: Product;
-  pagination: pagination;
+  extra?: LoveTestData | FreeTestData;
+  seller_id?: number | null;
+  repliesCount?: number;
+  product?: Product;
+  pagination?: pagination;
 }
 
 export interface Reply {
