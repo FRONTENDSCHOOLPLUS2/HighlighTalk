7b033184141ffb242d7bbc5151a7c64910e79754
woojoung <ywj981217@naver.com>
2024-08-23 16:46:01 +0900

    Feat: 스피너 수정 , 업로드 부분 css 수정 테스트 횟수 인트로 페이지 기능 구현#112

--- src/components/ MainCommPreviews/MainCommPreviewItem.tsx
+++ src/components/ MainCommPreviews/MainCommPreviewItem.tsx
@@ -20,9 +20,20 @@ function MainCommPreviewItem({ data }: MainThemesProps) {
       <Link href={`posts/${data._id}`}>
         <h1>{data.title}</h1>
         <p className="card-content">{data.content}</p>
-        <span>{data.views}</span>
-        <p>{data.repliesCount}</p>
-        <p>{data.createdAt.split(' ')[0]}</p>
+        <div className="card-footer">
+          <div className="footer-content">
+            <div className="icon-img n1"></div>
+            <span>{data.views}</span>
+          </div>
+          <div className="footer-content">
+            <div className="icon-img n2"></div>
+            <p>{data.repliesCount}</p>
+          </div>
+          <div className="footer-content">
+            <div className="icon-img n3"></div>
+            <p>{data.createdAt.split(' ')[0]}</p>
+          </div>
+        </div>
       </Link>
     </div>
   );
