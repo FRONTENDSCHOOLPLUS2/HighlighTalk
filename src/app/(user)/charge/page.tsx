6f7f0a75299a5a962ff0ca5a11857e029f980748
zldnlto <langchomi0@gmail.com>
2024-08-24 23:16:30 +0900

    Feat: 결제 페이지 메타 데이터 추가, 거래 내역 없을 경우 NoHistoryFound 컴포넌트 렌더링 #103

--- src/app/(user)/charge/page.tsx
+++ src/app/(user)/charge/page.tsx
@@ -2,6 +2,12 @@ import CoinNotice from '@/components/Coin/CoinNotice/CoinNotice';
 import './_ChargePage.scss';
 import CoinTabs from '@/components/Coin/CoinTabs/CoinTabs';
 
+export const metadata = {
+  title: '하이라이톡 | 코인 충전 페이지',
+  description: '하이라이톡 에서 AI기반 톡방 분석 서비스를 받아 보세요',
+  keywords: 'highlightalk',
+};
+
 function ChargePage() {
   return (
     <div className="charge-page-container">
