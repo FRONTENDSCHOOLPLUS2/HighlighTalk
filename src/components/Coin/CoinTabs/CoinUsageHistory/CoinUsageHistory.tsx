58b4f6140a2f13df53121411c607bc5a54c4699c
zldnlto <langchomi0@gmail.com>
2024-08-25 16:38:37 +0900

    Feat: 전체/충전/사용별 결제내역 렌더링 기능 구현 #103

--- src/components/Coin/CoinTabs/CoinUsageHistory/CoinUsageHistory.tsx
+++ src/components/Coin/CoinTabs/CoinUsageHistory/CoinUsageHistory.tsx
@@ -1,13 +1,16 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import { fetchOrderData } from '@/serverActions/orderAction';
 import { OrderDataType } from '@/types/order';
-import TransactionItem from './TransactionItem';
-import './_CoinUsageHistory.scss';
+import { LoadingSpinner } from '@/components/Spinner/Spinner';
 import NoHistoryFound from './NoHistoryFound';
+import './_CoinUsageHistory.scss';
+
+const TransactionItem = lazy(() => import('./TransactionItem'));
 
 function CoinUsageHistory() {
   const [orders, setOrders] = useState<OrderDataType[]>([]);
+  const [loading, setLoading] = useState(true);
   const [activeMenu, setActiveMenu] = useState('all');
 
   useEffect(() => {
@@ -16,47 +19,70 @@ function CoinUsageHistory() {
         const data = await fetchOrderData();
         console.log('fetchOrderData', data);
 
-        const orderArr: OrderDataType[] = [];
-        data.item.map((v) =>
-          orderArr.push({
-            ...v.order_info,
-            createdAt: v.createdAt,
-          })
-        );
+        const orderArr: OrderDataType[] = data.item.map((v) => ({
+          ...v.order_info,
+          createdAt: v.createdAt,
+        }));
 
         setOrders(orderArr);
       } catch (error) {
         console.error('Error fetching order data:', error);
+      } finally {
+        setLoading(false);
       }
     }
 
     fetchData();
   }, []);
 
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  const filteredOrders = orders.filter((order) => {
+    if (activeMenu === 'all') return true;
+    return order.order_type === activeMenu;
+  });
+
   return (
     <div className="coin-history-container">
-      {orders && orders.length > 0 ? (
-        <>
-          <ul className="transaction-filter">
-            <li>
-              <button className={`all active`}>전체</button>
-            </li>
-            <li>
-              <button className={`charge`}>충전</button>
-            </li>
-            <li>
-              <button className={`use`}>사용</button>
-            </li>
-          </ul>
-          <ul className="transactions-list">
-            {orders.map((order, index) => (
+      <ul className="transaction-filter">
+        <li>
+          <button
+            className={`all ${activeMenu === 'all' ? 'active' : ''}`}
+            onClick={() => setActiveMenu('all')}
+          >
+            전체
+          </button>
+        </li>
+        <li>
+          <button
+            className={`charge ${activeMenu === 'charge' ? 'active' : ''}`}
+            onClick={() => setActiveMenu('charge')}
+          >
+            충전
+          </button>
+        </li>
+        <li>
+          <button
+            className={`use ${activeMenu === 'use' ? 'active' : ''}`}
+            onClick={() => setActiveMenu('use')}
+          >
+            사용
+          </button>
+        </li>
+      </ul>
+      <ul className="transactions-list">
+        {filteredOrders.length > 0 ? (
+          <Suspense fallback={<p>Loading transactions...</p>}>
+            {filteredOrders.map((order, index) => (
               <TransactionItem order={order} key={index} index={index} />
             ))}
-          </ul>
-        </>
-      ) : (
-        <NoHistoryFound />
-      )}
+          </Suspense>
+        ) : (
+          <NoHistoryFound />
+        )}
+      </ul>
     </div>
   );
 }
