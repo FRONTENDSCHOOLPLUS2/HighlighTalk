9f1f7f3c6efb00ad4a9009cb74f7e44f9e330691
zldnlto <langchomi0@gmail.com>
2024-08-26 16:48:08 +0900

    Design: 마이페이지 기타 메뉴 스타일링 #114

--- src/app/(user)/mypage/AdditionalMenu.tsx
+++ src/app/(user)/mypage/AdditionalMenu.tsx
@@ -1,10 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 function AdditionalMenu() {
   return (
     <ul className="contents etc">
-      <li className="">공지사항</li>
-      <li className="">게시판</li>
-      <li className="">개발자에게 메일 보내기</li>
-      <li className="">Buy me a coffee</li>
+      <li>
+        <Link href="/posts/notice" className="notice">
+          <Image src="/image/clay/bell.png" width={70} height={70} alt="공지사항" />
+          <b>공지사항</b>
+        </Link>
+      </li>
+      <li className="">
+        <Link href="/posts/notice" className="notice">
+          <Image src="/image/clay/contract.png" width={70} height={70} alt="게시판 바로가기" />
+          <b>게시판</b>
+        </Link>
+      </li>
+      <li className="">
+        <Link href="mailto:highlightalk@gmail.com" className="notice">
+          <Image src="/image/clay/mail.png" width={70} height={70} alt="문의사항 메일 쓰기" />
+          <b>문의사항</b>
+        </Link>
+      </li>
+      <li className="">
+        <Link href="/posts/notice" className="notice">
+          <Image src="/image/clay/coffee.png" width={70} height={70} alt="buy me a coffee" />
+          <b>개발자 도와주기</b>
+        </Link>
+      </li>
     </ul>
   );
 }
