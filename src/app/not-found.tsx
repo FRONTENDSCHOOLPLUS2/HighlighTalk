cb5f61f893a3d6358a025658ce9d7bcd49ab63d4
woojoung <ywj981217@naver.com>
2024-08-27 00:26:42 +0900

    Feat: 에러,notfound 페이지 링크=>Button 컴포넌트로 변경 #127

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import '../styles/not-found.scss';
-import Link from 'next/link';
 import { poppinsFont } from '@/utils/font';
+import Button from '@/components/Button/Button';
+import { useRouter } from 'next/navigation';
 
 export default function Error() {
+  const router = useRouter();
   return (
     <div className={`${poppinsFont.className}`}>
       <div className="notFound-wrapper">
@@ -13,9 +15,14 @@ export default function Error() {
           <p className="text-p">앗, 찾을 수 없는 페이지 입니다.</p>
           <p className="text-p-r">요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하신것 같아요</p>
           <span className="text-p-l">We can't seem to find the page you're looking for.</span>
-          <Link href="/" className="tohome">
+          <Button
+            theme="secondary"
+            onClick={() => {
+              router.push('/');
+            }}
+          >
             ⚙️ 홈으로 돌아가기
-          </Link>
+          </Button>
         </div>
         <div className="notFound-image"></div>
       </div>
