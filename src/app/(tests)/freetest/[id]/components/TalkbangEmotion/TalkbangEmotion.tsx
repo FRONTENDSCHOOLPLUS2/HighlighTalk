41f384f99508724affc18d58266bbde7430a4a7e
woojoung <ywj981217@naver.com>
2024-08-27 00:10:04 +0900

    Feat: 사용하지 않는 파일삭제  #127

--- src/app/(tests)/freetest/[id]/components/TalkbangEmotion/TalkbangEmotion.tsx
+++ src/app/(tests)/freetest/[id]/components/TalkbangEmotion/TalkbangEmotion.tsx
@@ -1,42 +1,3 @@
-// 'use client';
-
-// import { Doughnut } from 'react-chartjs-2';
-// import './_TalkbangEmotion.scss';
-// import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
-
-// Chart.register(ArcElement, Tooltip, Legend);
-
-// type EmotionData = {
-//   [key: string]: number;
-// };
-
-// interface TalkbangEmotionProps {
-//   data: EmotionData;
-// }
-
-// const TalkbangEmotion: React.FC<TalkbangEmotionProps> = ({ data }) => {
-//   // Doughnut 차트 데이터 구성
-//   const chartData = {
-//     labels: Object.keys(data), // 감정 종류
-//     datasets: [
-//       {
-//         data: Object.values(data), // 각 감정의 값
-//         backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'],
-//       },
-//     ],
-//   };
-
-//   return (
-//     <div className="TalkbangEmotionCover">
-//       <div className="TalkbangEmotion">
-//         <Doughnut data={chartData} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TalkbangEmotion;
-
 'use client';
 
 import { Bar } from 'react-chartjs-2';
@@ -52,6 +13,7 @@ import {
 } from 'chart.js';
 
 // 필요한 요소들을 Chart.js에 등록
+// TODO: 예외처리 해야 할 듯
 Chart.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 type EmotionData = {
